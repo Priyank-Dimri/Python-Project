b0aa9e8fb83acc01fafb210d40416008847cb5a3
Priyank Dimri <dimri.priyank17@gmail.com>
2021-07-20 12:31:30 +0530

    First Python Program version1

---
+++ main.py
@@ -0,0 +1,34 @@
+# Addition program for positive numbers using loop, conditions, functions and modules
+
+import image
+
+# Simple add function which return value as per user input
+
+
+def my_add(num1, num2):
+    if (num1 or num2) < 1:
+        return "Not Positive"
+    else:
+        return num1+num2
+
+
+print(image.my_image)
+
+want_to_continue = 1
+print("Welcome to addition")
+
+# Using loop and conditional to check if the user want to continue or exit
+while want_to_continue == 1:
+    try:
+        number1 = int(input("Please input number 1: "))
+        number2 = int(input("Please input number 2: "))
+        addition = my_add(number1, number2)
+        print(f"Addition of 2 numbers through add function is {addition}")
+        want_to_continue = int(input("Do you want to continue, please enter 1 for continue or 0 to exit:"))
+        if want_to_continue == 0:
+            break
+    except ValueError:
+        print("you have not entered integer value")
+
+
+
